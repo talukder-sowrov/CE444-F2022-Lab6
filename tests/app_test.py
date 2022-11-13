5b168893b82741d3b86ee7a8ba556d39a6732034
talukder-sowrov <sowrov1000@gmail.com>
2022-11-13 17:56:33 -0500

    Linting and Code Formatting activity

--- tests/app_test.py
+++ tests/app_test.py
@@ -75,6 +75,7 @@ def test_messages(client):
     assert b"&lt;Hello&gt;" in rv.data
     assert b"<strong>HTML</strong> allowed here" in rv.data
 
+
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
     rv = client.get("/delete/1")
@@ -85,6 +86,7 @@ def test_delete_message(client):
     data = json.loads(rv.data)
     assert data["status"] == 1
 
+
 def test_search(client):
     """Ensure search functionality works properly"""
     login(client, app.config["USERNAME"], app.config["PASSWORD"])
@@ -93,5 +95,5 @@ def test_search(client):
         data=dict(title="test", text="<strong>HTML</strong> allowed here"),
         follow_redirects=True,
     )
-    rv = client.get('/search/?query=test')
+    rv = client.get("/search/?query=test")
     assert b"test" in rv.data
